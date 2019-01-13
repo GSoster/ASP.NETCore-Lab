3632157a2f998f2b273a5416bc0d9b5ae7a446a4
Guilherme Soster <GSoster@users.noreply.github.com>
2019-01-13 16:53:27 -0200

    Initial commit

---
+++ ASP.NET Core - Beginner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASP.NET_Core___Beginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
