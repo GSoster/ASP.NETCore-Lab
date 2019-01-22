7efc7efedcf13354ae69b40d5a8eeb09972d2a5b
Guilherme Soster <GSoster@users.noreply.github.com>
2019-01-22 12:27:51 -0200

    Created the List view (Index)

--- ASP.NET Core - Beginner/RazorPagesCrud/RazorPagesCrud/Pages/Index.cshtml.cs
+++ ASP.NET Core - Beginner/RazorPagesCrud/RazorPagesCrud/Pages/Index.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesCrud.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AppDbContext _db;
 
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
         }
+
+
+        public IList<Customer> Customers { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            //AsNoTracking is useful for reading scenarios, 
+            Customers = await _db.Customers.AsNoTracking().ToListAsync();
+        }
+
+        
     }
 }
