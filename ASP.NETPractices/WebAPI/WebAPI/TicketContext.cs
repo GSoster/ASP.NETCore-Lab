aa44c254196c438c60efb1c54ae846987f1f3bdd
Guilherme Soster <GSoster@users.noreply.github.com>
2019-02-06 14:44:48 -0200

    added new Project: WebAPI. Added Model class (TicketItem) Added DbContext (TicketContext) Added TicketContext to the DbContext on ConfigureServices (Startup.cs)

---
+++ ASP.NETPractices/WebAPI/WebAPI/TicketContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+using Microsoft.EntityFrameworkCore;
+namespace WebAPI
+{
+    public class TicketContext : DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options):base(options)
+        {
+
+        }
+
+        DbSet<TicketItem> TicketItems { get; set; }
+    }
+}
